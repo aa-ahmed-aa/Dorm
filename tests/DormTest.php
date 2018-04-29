cb84bb4458924c5abb174e91faff63e27363b9a9
Server <ahmedkhaled36@hotmail.com>
2018-04-29 20:42:28 +0200

    build single test for testing every response for run

--- tests/DormTest.php
+++ tests/DormTest.php
@@ -76,7 +76,7 @@ EOT;
 	/**
 	* 
 	*/
-  	public function test_run()
+  	public function test_run_accepted()
 	{
 		$input_file = [
 			'file_name' => 'in.txt',
@@ -101,17 +101,6 @@ EOT;
 "
 		];
 
-		$wrong_output_file = [
-			'file_name' => 'out.txt',
-			'file_content' => "9
-95
-15
-15
-9
-"
-		];
-
-
 		$correct_code = <<<'EOD'
 		#include <bits/stdc++.h>
 
@@ -134,27 +123,6 @@ int main()
 }
 EOD;
 		
-		$time_limit_code = <<<'EOD'
-		#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-    freopen("in.txt","r",stdin);
-    freopen("out.txt","w",stdout);
-    int n,x,y;
-
-    cin>>n;
-    while(true)
-    {
-        cout<<"ecco";
-    }
-
-    return 0;
-}
-EOD;
-
 		$obj = new Dorm();
 
 		$obj->setCompilationPath( TEST_COMPILER_DIR );
@@ -166,17 +134,120 @@ EOD;
 		//test eccepted
 		$run = $obj->run($input_file, $correct_output_file);
 		$this->assertTrue( $run == ACCEPTED );		
+	}
+
+	public function test_run_wrong_answer()
+	{
+		$input_file = [
+			'file_name' => 'in.txt',
+			'file_content' => "6
+4 5
+87 8
+8 7
+9 6
+4 5
+5 4
+"
+		];
+
+		$wrong_output_file = [
+			'file_name' => 'out.txt',
+			'file_content' => "9
+95
+15
+15
+9
+"
+		];
+
+		$correct_code = <<<'EOD'
+		#include <bits/stdc++.h>
+
+		using namespace std;
+
+		int main()
+		{
+		    freopen("in.txt","r",stdin);
+		    freopen("out.txt","w",stdout);
+		    int n,x,y;
+
+		    cin>>n;
+		    while(n--)
+		    {
+		        cin>>x>>y;
+		        cout<<x+y<<endl;
+		    }
+
+		    return 0;
+		}
+EOD;
+
+		$obj = new Dorm();
+		$obj->setCompilationPath( TEST_COMPILER_DIR );
+
+		//compile
+		$comp = $obj->compile( $correct_code, 'c++' );
+		$this->assertTrue( $comp );
 
 		//test wrong_answer
 		$run = $obj->run($input_file, $wrong_output_file);
 		$this->assertTrue( $run == WRONG_ANSWER );
+	}
+
+	public function test_run_time_limit_exceeded()
+	{
+		$input_file = [
+			'file_name' => 'in.txt',
+			'file_content' => "6
+4 5
+87 8
+8 7
+9 6
+4 5
+5 4
+"
+		];
+
+		$correct_output_file = [
+			'file_name' => 'out.txt',
+			'file_content' => "9
+95
+15
+15
+9
+9
+"
+		];
+
+		$time_limit_code = <<<'EOD'
+		#include <bits/stdc++.h>
+
+		using namespace std;
+
+		int main()
+		{
+		    freopen("in.txt","r",stdin);
+		    freopen("out.txt","w",stdout);
+		    int n,x,y;
+
+		    cin>>n;
+		    while(true)
+		    {
+		        cout<<"ecco";
+		    }
+
+		    return 0;
+		}
+EOD;
+
+		$obj = new Dorm();
+		$obj->setCompilationPath( TEST_COMPILER_DIR );
 
 		//test time_limit_exceeded
 		$comp = $obj->compile( $time_limit_code, 'c++' );
 		$this->assertTrue( $comp );
 
 		$time = $obj->run( $input_file, $correct_output_file );
-		// die($time . "Ahmed Khaled");
 		$this->assertTrue( $time == TIME_LIMIT_EXCEEDED );
 	}
 }
