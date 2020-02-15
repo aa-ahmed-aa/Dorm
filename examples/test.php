60a765f040f2db484fadb6e37884088d94f0d8af
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2020-02-15 03:27:10 +0200

    refactor the Dorm() to Executor() 🌞🌞

--- examples/test.php
+++ examples/test.php
@@ -1,6 +1,6 @@
 <?php
 require_once('./vendor/autoload.php');
-require_once('./src/list_of_defines.php');
+
 use Ahmedkhd\Dorm\Executor;
 
 $obj = new Executor();
@@ -38,7 +38,7 @@ public static void main(String[] args) {
 EOT;
 
 
-$comp = $obj->compile( $java_code, "java" );
+$comp = $obj->compile($java_code, "java");
 echo "Compilation : " . ( ! is_array($comp) ? "Success" : "Fail" )  . "\n";
 echo "Running is : " . ( ! is_array($comp) ? $obj->run() : "Fail" ) . "\n";
 
@@ -48,8 +48,8 @@ $python_code = <<<'EOT'
 print "Hello, Python3.4"
 EOT;
 
-$comp = $obj->compile( $python_code, "python2" );
-echo "Running : " . implode( $comp )  . "\n";
+$comp = $obj->compile($python_code, "python2");
+echo "Running : " . implode($comp) . "\n";
 
 
 ?>
