3d41385be4252b70b837ddb208b66eb5a9c32496
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2020-02-16 02:23:50 +0200

    List of defines added C,CPP,JAVA,PYTHON 🗿

--- src/Defines/list_of_defines.php
+++ src/Defines/list_of_defines.php
@@ -11,12 +11,21 @@ if (!defined('TIME_LIMIT_EXCEEDED')) {
 if (!defined('COMPILER_ERROR')) {
     define('COMPILER_ERROR', 3);
 }
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
+if (!defined('CPP')) {
+    define('CPP', 'cpp');
+}
+if (!defined('C')) {
+    define('C', 'c');
+}
+if (!defined('JAVA')) {
+    define('JAVA', 'java');
+}
+if (!defined('PYTHON')) {
+    define('PYTHON', 'python');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
 if (!defined('TEST_COMPILER_DIR')) {
-    define('TEST_COMPILER_DIR', __DIR__.DS.'..'.DS.'tests'.DS.'testDir');
+    define('TEST_COMPILER_DIR', __DIR__ . DS . '..' . DS . '..' . DS . 'tests' . DS . 'testDir');
 }
